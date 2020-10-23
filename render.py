d5d0e8cbae6a2f6036d42e7674bc85a17462723d
Colm Britton <colmjude@gmail.com>
2020-10-23 07:31:59 +0100

    Render any project content pages

--- render.py
+++ render.py
@@ -9,6 +9,8 @@ from frontmatter import Frontmatter
 from bin.govukify import govukify_markdown_output
 from digital_land_frontend.filters import make_link
 
+from markdown.extensions.toc import TocExtension
+
 docs = "docs/"
 
 
@@ -44,11 +46,12 @@ env.filters["make_link"] = make_link
 # get templates
 index_template = env.get_template("index.html")
 project_template = env.get_template("project.html")
+content_template = env.get_template("content.html")
 
 project_dir = "projects/"
 
 # init markdown
-md = markdown.Markdown()
+md = markdown.Markdown(extensions=[TocExtension(toc_depth="2-3")])
 
 
 def compile_markdown(md, s):
@@ -69,20 +72,40 @@ def get_project_content(filename):
     }
 
 
+def markdown_files_only(files, file_ext=".md"):
+    return [f for f in files if f.endswith(file_ext)]
+
+
 for project in projects:
     hasMultipleDatasets = False
     project_content = get_project_content(f"{project_dir}{project}/index.md")
+    # get dataset content to display on project page
     if os.path.isdir(f"{project_dir}{project}/datasets"):
         datasets = []
-        md_files = [
-            f
-            for f in os.listdir(f"{project_dir}{project}/datasets")
-            if f.endswith(".md")
-        ]
+        md_files = markdown_files_only(os.listdir(f"{project_dir}{project}/datasets"))
+
         for f in md_files:
             datasets.append(get_project_content(f"{project_dir}{project}/datasets/{f}"))
         project_content["datasets"] = datasets
+
+    # render any additional content pages
+    if os.path.isdir(f"{project_dir}{project}/content"):
+        md_files = markdown_files_only(os.listdir(f"{project_dir}{project}/content"))
+
+        for f in md_files:
+            file_content = Frontmatter.read_file(f"{project_dir}{project}/content/{f}")
+            html = compile_markdown(md, file_content["body"])
+            render(
+                f"project/{project}/{f.replace('.md', '')}/index.html",
+                content_template,
+                content=html,
+                toc=md.toc_tokens,
+                fm=file_content["attributes"],
+                project=project,
+            )
+
     render(f"project/{project}/index.html", project_template, project=project_content)
+
 
 # generate summary for index page
 summary = {}
